d3baf6cafc870465be251434257b08875d20b95e
brherr <herrbrad@yahoo.com>
2025-03-13 09:49:51 -0400

    fix lint in __root

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,6 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { Outlet, createRootRouteWithContext } from "@tanstack/react-router";
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 import { UserT } from "@/lib/store";
 
 // TODO: add dotenv logic to conditionally render devtools
@@ -10,7 +9,6 @@ export const Route = createRootRouteWithContext<{ user?: UserT }>()({
     return (
       <>
         <Outlet />
-        {/* <TanStackRouterDevtools /> */}
       </>
     );
   },
